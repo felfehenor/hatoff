283a82e9f7fcec89b244bcd3eace97a081f84a8d
Kyle J. Kemp <kyle@seiyria.com>
2024-11-05 12:13:25 -0600

    visual fixes

--- src/app/pages/game/game.component.ts
+++ src/app/pages/game/game.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, ROUTES } from '@angular/router';
+import { RouterLink, RouterOutlet, ROUTES } from '@angular/router';
 import { gameRoutes } from '../../game.routes';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   providers: [
     {
       provide: ROUTES,
