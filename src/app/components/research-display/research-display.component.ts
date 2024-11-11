96bf336a88f9bbe4dd67ba7bf9805157a5b63845
Kyle J. Kemp <kyle@seiyria.com>
2024-11-11 07:20:36 -0600

    add icon for active research

--- src/app/components/research-display/research-display.component.ts
+++ src/app/components/research-display/research-display.component.ts
@@ -1,11 +1,18 @@
 import { Component, computed, input } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroStar } from '@ng-icons/heroicons/outline';
 import { getResearchFor } from '../../helpers';
 import { GameResearch } from '../../interfaces';
 
 @Component({
   selector: 'app-research-display',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  providers: [
+    provideIcons({
+      heroStar,
+    }),
+  ],
   templateUrl: './research-display.component.html',
   styleUrl: './research-display.component.scss',
 })
