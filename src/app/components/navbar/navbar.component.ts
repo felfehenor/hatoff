1376e7605409421d6a0df247040bdaa26f0ad0e4
Kyle J. Kemp <kyle@seiyria.com>
2024-11-11 07:08:40 -0600

    closes #12

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component, computed } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroCog6Tooth } from '@ng-icons/heroicons/outline';
 import { gamestate, isSetup } from '../../helpers';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIconComponent],
+  providers: [provideIcons({ heroCog6Tooth })],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
